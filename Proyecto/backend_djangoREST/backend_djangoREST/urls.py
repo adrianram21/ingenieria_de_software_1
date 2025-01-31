00a11961e5d55af02e3e2e0195e7837f37e7cf0b
Adrian Ramirez Gonzalez <adramirez@unal.edu.co>
2025-01-31 14:25:13 -0500

    modificaciones Linter

--- Proyecto/backend_djangoREST/backend_djangoREST/urls.py
+++ Proyecto/backend_djangoREST/backend_djangoREST/urls.py
@@ -1,9 +1,13 @@
+"""
+Configuracion de URL's
+"""
+
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
-) 
+)
 
 
 urlpatterns = [
